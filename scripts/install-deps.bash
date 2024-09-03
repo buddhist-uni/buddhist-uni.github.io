8933509cc262b4eb904a0cfce73b8f18333d7aff
Khemarato Bhikkhu <theopenbuddhistuniversity@gmail.com>
2024-09-03 13:49:42 +0700

    Bump dependencies

--- scripts/install-deps.bash
+++ scripts/install-deps.bash
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Make sure to run:
-# npm install --no-bin-links OR npm ci
-# before running this script
+# Installs misc (non-gem) dependencies for the Jekyll build
+# Make sure to:
+#   1) run `npm install --no-bin-links` OR `npm ci`
+#      BEFORE running this script
+#   2) run this script from the root of the repo
 
 rm -rf assets/webfonts && \
   mkdir assets/webfonts && \
   cp node_modules/@fortawesome/fontawesome-free/webfonts/* assets/webfonts/ 
+
+wget https://buddhistuniversity.net/analytics/content/download_counts.json --output-document="_data/content_downloads.json"
 
 md5sum package-lock.json assets/css/*.scss _sass/*.scss _sass/minima/* | md5sum | head -c 7 > _data/cssCacheToken.yml
