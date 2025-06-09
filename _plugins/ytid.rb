361bb8db189d3640871dc44096ff26335ae99281
Khemarato Bhikkhu <theopenbuddhistuniversity@gmail.com>
2025-06-09 14:33:04 +0700

    fix /live/ ytid bug

--- _plugins/ytid.rb
+++ _plugins/ytid.rb
@@ -1,6 +1,6 @@
 module Jekyll
   module YTIDFilter
-    @@regex = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
+    @@regex = /(?:youtube(?:-nocookie)?\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?|live)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/
     def ytid(input)
       match = @@regex.match(input)
       if match && match[1].is_a?(String)
